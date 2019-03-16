66633e86634adb099bfe3068a2c82ede843766c4
Peter Ho <ostentatiousitus@gmail.com>
2019-03-15 20:42:30 -0400

    progress

--- src/Stage.java
+++ src/Stage.java
@@ -8,15 +8,22 @@ public class Stage {
 	private double			width, height;
 	private int				vbo	= 0;
 	private int				vao	= 0;
+	private int				ebo	= 0;
 	private ShaderProgram	shaderProgram;
 	
-	// private float vertices[] = { 
+	// private float vertices[] = {
 	// -1.0f, 1.0f, 1.0f, 1.0f, 1.0f, -1.0f, -1.0f,
 	// -1.0f };
 	float[] vertices = new float[] {
-			+0.0f, +540.0f, // Top coordinate
-			-960.0f, -540.0f, // Bottom-left coordinate
-			+960.0f, -540.0f // Bottom-right coordinate
+			// +0.0f, +540.0f, // Top coordinate
+			-940.0f, +520.0f, // Top-right coordinate
+			+940.0f, +520.0f, // Top-left coordinate
+			-940.0f, -520.0f, // Bottom-left coordinate
+			+940.0f, -520.0f // Bottom-right coordinate
+	};
+	
+	int indices[] = {
+			0, 1, 2, 1, 2, 3
 	};
 	
 	public Stage()
@@ -37,9 +44,14 @@ public class Stage {
 		glBindBuffer(GL_ARRAY_BUFFER, vbo);
 		glBufferData(GL_ARRAY_BUFFER, verticesBuffer, GL_STATIC_DRAW);
 		
+		// What order to use vertices (specified in indicies)
+		ebo = glGenBuffers();
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices, GL_STATIC_DRAW);
+
+		// Describe how to interpret vertices from array
 		glVertexAttribPointer(0, 2, GL_FLOAT, false, 0, 0);
 		glBindVertexArray(0);
-		
 	}
 	
 	public void dispose()
@@ -59,13 +71,16 @@ public class Stage {
 	public void render()
 	{
 		// glClear(GL_COLOR_BUFFER_BIT);
+		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 		
 		shaderProgram.bind();
 		
 		glBindVertexArray(vao);
 		glEnableVertexAttribArray(0);
 		
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+//		glDrawArrays(GL_TRIANGLES, 0, 6);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		
 		glDisableVertexAttribArray(0);
 		glBindVertexArray(0);
